7e49cc4e50f8416f9a725fae8321624220b31bd1
faizan-devstack <faizan.devstack@gmail.com>
2025-01-22 13:06:57 +0500

    5th commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Muhammad Faizan Maqbool",
+  title: "Muhammad Faizan",
   description: "My Portfolio",
 };
 
